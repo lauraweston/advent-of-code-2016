6f6dce40def68b6f6cbb2238b5db6d52d2a3a879
Laura Weston <laura.weston@outlook.com>
2017-01-01 19:00:56 +0000

    Day 3 part two completed

--- day3/day3.js
+++ day3/day3.js
@@ -1,23 +1,56 @@
 'use strict';
 
-function isValidTriangle(input) {
-    const sides = input.trim().split(/\s+/);
-    const sideLengths = sides.map(side => parseInt(side, 10));
+function parseTriangleRow(row) {
+    return row
+        .trim()
+        .split(/\s+/)
+        .map(side => parseInt(side, 10));
+}
+
+function parse(input) {
+    return input.split(/\n/).map(parseTriangleRow);
+}
+
+function isValidTriangle(sideLengths) {
     sideLengths.sort((a, b) => a - b);
     return sideLengths[0] + sideLengths[1] > sideLengths[2];
 }
 
-function parse(input) {
-    return input.split(/\n/);
-}
-
-function countValidTriangles(input) {
+function countValidTrianglesByRow(input) {
     const triangles = parse(input);
     return triangles.filter(isValidTriangle).length;
+}
+
+function transformSetOfThreeTriangleSides(setOfThreeTriangleSides) {
+    const transformedTriangles = [[], [], []];
+    setOfThreeTriangleSides.forEach(sides => {
+        for (let i = 0; i < sides.length; i++) {
+            transformedTriangles[i].push(sides[i]);
+        }
+    });
+    return transformedTriangles;
+}
+
+function splitSidesIntoSetsOfThree(sides) {
+    const transformedTriangles = [];
+    while (sides.length > 2) {
+        let setOfThreeTriangleSides = sides.splice(0, 3);
+        transformSetOfThreeTriangleSides(setOfThreeTriangleSides)
+            .forEach(triangle => {
+                transformedTriangles.push(triangle);
+            });
+    }
+    return transformedTriangles;
+}
+
+function countValidTrianglesByColumn(input) {
+    const sides = parse(input);
+    return splitSidesIntoSetsOfThree(sides).filter(isValidTriangle).length;
 }
 
 module.exports = {
     isValidTriangle,
     parse,
-    countValidTriangles
+    countValidTrianglesByRow,
+    countValidTrianglesByColumn
 };
--- test/day3Test.js
+++ test/day3Test.js
@@ -3,15 +3,14 @@ const fs = require("fs");
 const day3 = require("../day3/day3.js");
 const isValidTriangle = day3.isValidTriangle;
 const parse = day3.parse;
-const countValidTriangles = day3.countValidTriangles;
+const countValidTrianglesByRow = day3.countValidTrianglesByRow;
+const countValidTrianglesByColumn = day3.countValidTrianglesByColumn;
 
 describe("isValidTriangle", () => {
     describe("given the length of 3 sides", () => {
         const testCases = [
-            ["1 1 1", true],
-            ["  1  1 1  ", true],
-            ["25 10 5", false],
-            ["  25 10   5 ", false]
+            [[1, 1, 1], true],
+            [[25, 10, 5], false],
         ];
         testCases.forEach(testCase => {
             it("confirms if the sides make up a valid triangle", () => {
@@ -25,7 +24,7 @@ describe("isValidTriangle", () => {
 
 describe("parse input", () => {
     const testCases = [
-        ["1 1 1\n1 1 1", ["1 1 1", "1 1 1"]]
+        ["  1  1 1\n25 5 10", [[1, 1, 1], [25, 5, 10]]]
     ];
 
     testCases.forEach(testCase => {
@@ -37,14 +36,29 @@ describe("parse input", () => {
     });
 });
 
-describe("Valid triangle counter", () => {
+describe("Horizontal valid triangle counter", () => {
     const testCases = [
         ["1 1 1\n1 1 1", 2],
         [fs.readFileSync(__dirname + "/puzzleInput.txt", "utf8"), 993]
     ];
-     testCases.forEach(testCase => {
-        it("counts number of valid triangles given a list of sides", () => {
-            const actual = countValidTriangles(testCase[0]);
+    testCases.forEach(testCase => {
+        it("counts number of valid triangles given a list of sides grouped by row", () => {
+            const actual = countValidTrianglesByRow(testCase[0]);
+            const expected = testCase[1];
+            assert.equal(actual, expected);
+        });
+    });
+});
+
+describe("Vertical valid triangle counter", () => {
+    const testCases = [
+        ["2 2 2\n3 3 3\n4 4 4", 3],
+        ["2 2 2\n3 3 3\n4 4 4\n2 2 2\n3 3 3\n4 4 4", 6],
+        [fs.readFileSync(__dirname + "/puzzleInput.txt", "utf8"), 1849]
+    ];
+    testCases.forEach(testCase => {
+        it("counts number of valid triangles given a list of sides grouped in vertical threes", () => {
+            const actual = countValidTrianglesByColumn(testCase[0]);
             const expected = testCase[1];
             assert.equal(actual, expected);
         });
